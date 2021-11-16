daa7d5d4aa52ae54ed05165f783fbb66e0211306
tahniyat-nisar <93429497+tahniyat-nisar@users.noreply.github.com>
2021-11-16 18:52:05 +0530

    Add files via upload

---
+++ print only last element in list.py
@@ -0,0 +1,7 @@
+a=["python",25,3.0,True]
+index=0
+while index<len(a):   # or (len(a)-1) if you take then no need to take if condition
+    # index=index+1
+    if index==3:
+        print(type(a[index]))
+    index=index+1
