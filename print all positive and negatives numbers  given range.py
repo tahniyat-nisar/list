89fc1bf48996a7ab51ea8a88736dcca41d2e7d49
tahniyat-nisar <93429497+tahniyat-nisar@users.noreply.github.com>
2021-11-18 19:40:04 +0530

    Update and rename print all positive and negatives numbers .py given range. to print all positive and negatives numbers  given range.py

--- print all positive and negatives numbers  given range.py
+++ print all positive and negatives numbers  given range.py
@@ -16,3 +16,17 @@ i=0
 while i in a:
     print(i,end=", ")
     i=i+1
+
+
+
+
+a=range(-4,5)
+i=4
+while i<len(a):
+    print(a[i],end=", ")
+    if a[i]==4:
+        c=a[i]+1
+        print(c)
+    i=i+1
+# Input: start = -4, end = 5
+# Output: 0, 1, 2, 3, 4, 5
