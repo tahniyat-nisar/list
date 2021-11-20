64c526b7c9530fb513532ef2d040ac9788be5c69
tahniyat-nisar <93429497+tahniyat-nisar@users.noreply.github.com>
2021-11-21 00:35:46 +0530

    Update find out Duplicates in the list.py

--- find out Duplicates in the list.py
+++ find out Duplicates in the list.py
@@ -14,5 +14,35 @@ while i<len(n):
     i=i+1
 print(c)
 
-
 # Output:[19,17,12]
+
+
+
+
+
+
+
+
+
+
+
+
+# extract all elements whose frequency is greater than K.
+# Input: test_list = [4, 6, 4, 3, 3, 4, 3, 4, 3, 8], K = 3
+# Output: [4, 3]
+
+n = [4, 6, 4, 3, 3, 4, 3, 4, 3, 8] 
+i=0
+c=[]
+while i<len(n):
+    j=0
+    count=0
+    while j<len(n):
+            if n[i]==n[j]:
+                count=count+1
+                if count>3:
+                    if n[i] not in c:
+                        c.append(n[i])                    
+            j=j+1  
+    i=i+1
+print(c)
