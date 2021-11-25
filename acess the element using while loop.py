3dc4c22c8e24beca8fcb5c9b6902d916281536fd
tahniyat-nisar <93429497+tahniyat-nisar@users.noreply.github.com>
2021-11-25 19:38:24 +0530

    Update acess the element using while loop.py

--- acess the element using while loop.py
+++ acess the element using while loop.py
@@ -3,3 +3,14 @@ index=0
 while index<len(a):
     print(index,":",a[index])
     index=index+1
+
+    
+    
+    
+a=["python",25,3.0,True]
+index=0
+while index<len(a):   # or (len(a)-1) if you take then no need to take if condition
+    # index=index+1
+    if index==3:     #here we are trying to acess particular indexin element
+        print(type(a[index]))
+    index=index+1
