732ec6ccf5c718a0266dce5911b1f2e2201f33f5
tahniyat-nisar <93429497+tahniyat-nisar@users.noreply.github.com>
2021-12-04 16:17:28 +0530

    Update binary to decimal without using count and reverse function.py

--- binary to decimal without using count and reverse function.py
+++ binary to decimal without using count and reverse function.py
@@ -10,3 +10,19 @@ while index<count and count<length:
     index=index-1
     count=count+1
 print(sum)
+
+
+# binary=int(input("enter a binary number:"))
+# B=str(binary)
+# c=list(B)
+# length=len(c)
+# i=length-1
+# count=0
+# sum=0
+# while i>=0 and count<=length:
+#       cal=int(c[i])*(2**count)
+#       sum=sum+cal      
+#       count+=1
+#       i-=1
+# print(sum)
+# print(cal)
